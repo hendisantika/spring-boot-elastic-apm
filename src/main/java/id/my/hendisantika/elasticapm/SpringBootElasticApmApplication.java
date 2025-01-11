b2fd704f3413f40b80c34b34b8b02fcbd0ebdd0e
hendisantika <hendisantika@yahoo.co.id>
2025-01-11 08:20:11 +0700

    Add @EnableScheduling🫸🌀✏️📗🐧🐳⬆

--- src/main/java/id/my/hendisantika/elasticapm/SpringBootElasticApmApplication.java
+++ src/main/java/id/my/hendisantika/elasticapm/SpringBootElasticApmApplication.java
@@ -2,8 +2,10 @@ package id.my.hendisantika.elasticapm;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class SpringBootElasticApmApplication {
 
     public static void main(String[] args) {
